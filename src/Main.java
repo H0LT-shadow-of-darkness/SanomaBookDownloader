e830fb592de36b576d49fb71cf5ad72b26c7d9c0
H0LT <86878455+H0LT-shadow-of-darkness@users.noreply.github.com>
2024-05-21 17:32:11 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
 	public static void main(String[] args) throws URISyntaxException, IOException, DocumentException {
 		
-		String url1="https://d38l3k3yaet8r2.cloudfront.net/resources/products/epubs/generated/"+"77FDB511-4AAA-478D-8C56-B661675CD049"+"/foxit-assets/pages/page";
+		String url1="https://d38l3k3yaet8r2.cloudfront.net/resources/products/epubs/generated/"+"CHANGE THIS WITH TEH UUID OF YOUR BOOK'S LINK"+"/foxit-assets/pages/page";
 		String url2="?password=&accessToken=null&formMode=true";
-		int pagine = 0;
-		String titolo = "";
+		int pagine = 0; //number of pages (find this in the last page's link)
+		String titolo = ""; //choose a title for your pdf book
 		new Book_Downloader(url1,url2,pagine,titolo);
 
 	}
